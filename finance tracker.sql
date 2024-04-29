deb10624cf30001eb746ada9bc2fb67fb5dfa4bc
Janith Jayashan <123491619+janithjay@users.noreply.github.com>
2024-04-29 15:36:58 +0530

    Update finance tracker.sql

--- finance tracker.sql
+++ finance tracker.sql
@@ -18,7 +18,19 @@ income_id int NOT Null AUTO_INCREMENT PRIMARY KEY,
 user_name char(50) NOT NULL ,
 amount FLOAT(15,2) NOT NULL check (amount>0),
 category char(50) NOT NULL,
-`date`  Date,
+`date` DATE,
+notes VARCHAR(255),
+constraint fk1 foreign key (user_name) references user_table(user_name)
+
+);
+
+CREATE TABLE expenses(
+expenses_id int NOT Null AUTO_INCREMENT PRIMARY KEY,
+user_name char(50) NOT NULL ,
+amount FLOAT(15,2) NOT NULL check (amount>0),
+category char(50) NOT NULL,
+`date` DATE,
+notes VARCHAR(255),
 constraint fk1 foreign key (user_name) references user_table(user_name)
 
 );
